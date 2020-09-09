62024b184a4a52fcffd5540d2cb84d66eef46514
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-09-09 00:22:47 -0400

    added db3

--- data/migrations/20200908231412_create_cars_table.js
+++ data/migrations/20200908231412_create_cars_table.js
@@ -1,8 +1,17 @@
-
+// records changes to the db schema
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("car", tbl => {
+    tbl.increments('id');
+    tbl.string('vin').unique().notNullable().index();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.string('mileage').notNullable();
+  })
 };
 
+//undo changes defined on the up func
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
+
+// VIN, make, model and mileage
